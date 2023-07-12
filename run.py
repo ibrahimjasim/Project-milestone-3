e45dc4c603dde808b3c470f4222e8f17feb05f97
ibrahimjasim <>
2023-07-12 20:37:40 +0000

    Define two patterns of size 8

---
+++ run.py
@@ -0,0 +1,7 @@
+# Import randint function from random module
+from random import randint
+
+Hidden_Pattern = [[' ']*8 for x in range(8)]
+Guess_Pattern = [[' ']*8 for x in range(8)]
+
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
