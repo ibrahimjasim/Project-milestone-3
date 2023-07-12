d7f63f409fac47766ca1fafcd9a61a7de2f59909
ibrahimjasim <>
2023-07-12 21:14:44 +0000

    Defining the function of the ship's location.

--- run.py
+++ run.py
@@ -18,3 +18,22 @@ def print_board(board):
         row_num += 1
 
 
+def get_ship_location():
+    let_to_num = {'A': 0, 'B': 1, 'C': 2,
+                  'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+    # Enter the row number between 1 to 8
+    row = input("Please enter a ship row 1-8: ")
+
+    while row not in '12345678':
+        print("Please enter a valid row")
+        row = input('Please enter a ship row 1-8: ')
+
+    # Enter the Ship column from A to H
+    column = input('Please enter a ship column A-H: ').upper()
+
+    while column not in 'ABCDEFGH':
+        print("Please enter a valid column")
+        column = input('Please enter a ship column A-H: ')
+
+    return int(row) - 1, let_to_num[column]
+
