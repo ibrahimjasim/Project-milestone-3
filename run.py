7032c72d8f05dec82fdf59a992498e2e9b2c44fc
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-01-19 19:06:27 +0000

    Update the mappings for columns

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ Computer_Hidden_Pattern = [[' '] * 8 for _ in range(8)]
 Computer_Guess_Pattern = [[' '] * 8 for _ in range(8)]
 
 
+# Mappings for columns
+let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def print_board(board):
     print('   A B C D E F G H')
