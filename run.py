bd76b1be52b605ea4cfc6293ac96a59ca534926d
IsharaNawarathna <e17219@eng.pdn.ac.lk>
2024-03-03 22:09:31 +0530

    added docstrings to the code

--- run.py
+++ run.py
@@ -1,18 +1,30 @@
 from random import randint
 
+
 # Initialize game boards for both player and computer
-Player_Hidden_Pattern = [[' '] * 8 for _ in range(8)]
-Player_Guess_Pattern = [[' '] * 8 for _ in range(8)]
-Computer_Hidden_Pattern = [[' '] * 8 for _ in range(8)]
-Computer_Guess_Pattern = [[' '] * 8 for _ in range(8)]
+player_hidden_pattern = [[' '] * 8 for _ in range(8)]
+player_guess_pattern = [[' '] * 8 for _ in range(8)]
+computer_hidden_pattern = [[' '] * 8 for _ in range(8)]
+computer_guess_pattern = [[' '] * 8 for _ in range(8)]
 
 
-# Mappings for columns
-let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-# Function to print two game boards side by side
-def print_boards(board1, board2, board1_title="Your Guesses", board2_title="Computer's Guesses"):
+def print_boards(board1, board2, 
+                 board1_title="Your Guesses", board2_title="Computer's Guesses"):
+    '''
+        Function to print two game boards side by side
+
+        input : 
+            board1 : list of characters related to the user
+            board2 : list of characters related to the computer
+            board1_title : title of the user board
+            board2_title : title of the computer board
+
+        output : 
+            prints the title of two boards and then the boards themselves
+
+    '''
     print(f'{board1_title:20s}    {board2_title}')
     print('   A B C D E F G H      A B C D E F G H')
     print('   ****************     ****************')
@@ -23,19 +35,37 @@ def print_boards(board1, board2, board1_title="Your Guesses", board2_title="Comp
 
 
 def get_row_from_user():
+    '''
+        Get the row as a input from the user and then
+        validate the user input. If it is not correct,
+        let the user again enter the row. Once a valid 
+        input is detected, convert that to an int and return.
+    '''
+
     while True:
         row = input('Please enter a ship row (1-8): ')
         if row not in ['1','2','3','4','5','6','7','8']:
-            print("Please enter a valid row(1<= row <= 8)")
+            print("Please enter a valid row ( 1<= row <= 8 ) ")
             continue
         else:
             return int(row)-1
 
 def get_col_from_user():
+    '''
+        Get the column as a input from the user and then
+        validate the user input. If it is not correct,
+        let the user again enter the row. Once a valid 
+        input is detected, get the corresponding index value and return.
+
+    '''
+
+    # Mappings for columns
+    let_to_num = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
     while True:
         column = input('Please enter a ship column (A-H): ').upper()
         if column not in ['A','B','C','D','E','F','G','H']:
-            print("Please enter a valid column(A <= column <= H)")
+            print("Please enter a valid column ( A <= column <= H ) ")
             continue
         else:
             return let_to_num[column]
@@ -63,29 +93,29 @@ def play_battleship():
     print('Welcome to Battleship')
 
     # Setting different difficulties: 5 ships for player, 3 ships for computer
-    create_ships(Player_Hidden_Pattern, 5)
-    create_ships(Computer_Hidden_Pattern, 5)
+    create_ships(player_hidden_pattern, 5)
+    create_ships(computer_hidden_pattern, 5)
     turns = 10
     player_hits = 0
     computer_hits = 0
 
     while turns > 0:
-        print_boards(Player_Guess_Pattern, Computer_Guess_Pattern)
+        print_boards(player_guess_pattern, computer_guess_pattern)
         print()
 
         # Player's turn
         row = get_row_from_user()
         column = get_col_from_user()
 
-        if Player_Guess_Pattern[row][column] == '-':
+        if player_guess_pattern[row][column] == '-':
             print('You already guessed that.')
-        elif Computer_Hidden_Pattern[row][column] == 'X':
+        elif computer_hidden_pattern[row][column] == 'X':
             print('Congratulations! You hit a battleship!')
-            Player_Guess_Pattern[row][column] = 'X'
+            player_guess_pattern[row][column] = 'X'
             player_hits += 1
         else:
             print('Sorry, you missed.')
-            Player_Guess_Pattern[row][column] = '-'
+            player_guess_pattern[row][column] = '-'
         turns -= 1
 
         if player_hits == 5:  # Win condition based on computer's ships
@@ -98,15 +128,15 @@ def play_battleship():
         # Computer's turn
         comp_row, comp_column = get_computer_location()
 
-        if Computer_Guess_Pattern[comp_row][comp_column] in ['X', '-']:
+        if computer_guess_pattern[comp_row][comp_column] in ['X', '-']:
             continue
-        elif Player_Hidden_Pattern[comp_row][comp_column] == 'X':
+        elif player_hidden_pattern[comp_row][comp_column] == 'X':
             print("Oh no! The computer hit your battleship!")
-            Computer_Guess_Pattern[comp_row][comp_column] = 'X'
+            computer_guess_pattern[comp_row][comp_column] = 'X'
             computer_hits += 1
         else:
             print("The computer missed!")
-            Computer_Guess_Pattern[comp_row][comp_column] = '-'
+            computer_guess_pattern[comp_row][comp_column] = '-'
 
         if computer_hits == 5:  # Computer's win condition
             print("Oh no! The computer has sunk all your battleships.")
@@ -114,7 +144,7 @@ def play_battleship():
 
     if turns == 0:
         print('Game Over')
-        print_boards(Player_Guess_Pattern, Computer_Guess_Pattern)
+        print_boards(player_guess_pattern, computer_guess_pattern)
         if player_hits > computer_hits:
             print("Congratulations! You have won with more hits.")
         elif computer_hits > player_hits:
